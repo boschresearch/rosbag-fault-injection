e3e5e9c6f92678ca85a5ce30e3d8c2e7b9e416d5
Jo Min Hee <MinHeeJo@de.bosch.com>
2026-03-06 12:32:45 +0100

    Update header
    Signed-off-by: Jo Min Hee <MinHeeJo@de.bosch.com>

--- src/plot.py
+++ src/plot.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 #
-# Copyright [2026] [Min Hee Jo MinHee.Jo@de.bosch.com]
+# Copyright (c) 2024 Robert Bosch GmbH and its subsidiaries.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not  use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+__author__ = "Min Hee Jo"
+__copyright__ = "Copyright 2024, Robert Bosch GmbH"
+__license__ = " Apache"
+__version__ = "2.0"
+__email__ = minhee.jo@de.bosch.com
 
-#    http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 from rosbags.rosbag2 import Reader
 from rosbags.typesys import Stores, get_types_from_msg, get_typestore
