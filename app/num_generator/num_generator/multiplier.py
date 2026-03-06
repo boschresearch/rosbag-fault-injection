e862f4adbe34146bc3d2caf2773a4356415c25ec
Jo Min Hee <MinHeeJo@de.bosch.com>
2026-03-06 11:14:00 +0100

    Update the header
    Signed-off-by: Jo Min Hee <MinHeeJo@de.bosch.com>

--- app/num_generator/num_generator/multiplier.py
+++ app/num_generator/num_generator/multiplier.py
@@ -1,17 +1,13 @@
 """" This node is used to multiply the number by a factor of 2 """
 #!/usr/bin/env python3
 #
-# Copyright (c) 2024 Robert Bosch GmbH and its subsidiaries.
-# This program and the accompanying materials are made available under
-# the terms of the Bosch Internal Open Source License v4
-# which accompanies this distribution, and is available at
-# http://bios.intranet.bosch.com/Apache-2.0.txt
+# Copyright [2026] [Min Hee Jo MinHee.Jo@de.bosch.com]
 
-__author__ = "Min Hee Jo"
-__copyright__ = "Copyright 2024, Robert Bosch GmbH"
-__license__ = "BIOSL"
-__version__ = "4.0"
-__email__ = "minhee.jo@de.bosch.com"
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 import rclpy
 import time
--- src/inject.sh
+++ src/inject.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Copyright (c) 2024 Robert Bosch GmbH and its subsidiaries.
-# This program and the accompanying materials are made available under
-# the terms of the Bosch Internal Open Source License v4
-# which accompanies this distribution, and is available at
-# http://bios.intranet.bosch.com/Apache-2.0.txt
-#
-# Author        : Min Hee Jo
-# Email         : minhee.jo@de.bosch.com
+# Copyright [2026] [Min Hee Jo MinHee.Jo@de.bosch.com]
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
 # Usage         : bash inject.sh -h
 # Description   : Inject faults to rosbags and plot the results according to user options and config.yaml
 
