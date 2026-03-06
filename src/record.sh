fa9311c1bebe4a283bfaa3dd9f9f81c4d360093a
Jo Min Hee <MinHeeJo@de.bosch.com>
2026-03-06 11:14:00 +0100

    Update the header
    Signed-off-by: Jo Min Hee <MinHeeJo@de.bosch.com>

--- src/record.sh
+++ src/record.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #
-# Copyright (c) 2024 Robert Bosch GmbH and its subsidiaries.
-# This program and the accompanying materials are made available under
-# the terms of the Bosch Internal Open Source License v4
-# which accompanies this distribution, and is available at
-# http://bios.intranet.bosch.com/Apache-2.0.txt
-#
-# Author        : Min Hee Jo
-# Email         : minhee.jo@de.bosch.com
-# Usage         : bash record.sh -h
-# Description   : Record a new rosbag with user options
+# Copyright [2026] [Min Hee Jo MinHee.Jo@de.bosch.com]
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 # define usage function
 usage() {
